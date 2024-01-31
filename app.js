0532c5aa369afc68501d377349a03259931adfd3
Asemahle Nkwali <99318798+TEARN1@users.noreply.github.com>
2024-01-31 10:55:10 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+
+function switchBtn() {
+    document.getElementById("Switch").style.display = "none"
+}
+
+function menuBtn() {
+    document.getElementById("MenuMenu").style.display = "block";
+}
+
+function MenuMenuX() {
+    document.getElementById("MenuMenu").style.display = "none";
+}
+
+// close form section
+
+function formSectionClose() { 
+    document.getElementById("formSection").style.display = "none"
+ }
+
+// Open form section 
+
+ function Talktomesection() { 
+    document.getElementById("formSection").style.display = "block"
+ }
